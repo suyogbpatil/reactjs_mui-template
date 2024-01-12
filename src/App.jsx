118287a7cf3ee0082fbef1111bbbab51e9d1cb98
suyogbpatil <suyogbpatil@yahoo.com>
2024-01-12 15:28:21 +0530

    project template udpated

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+function App() {
+  return (
+    <>
+      <h3>Template Project</h3>
+    </>
+  );
+}
+
+export default App;
